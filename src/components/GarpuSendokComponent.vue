e4c754782e51846bac31f2141965f2d224de61d6
Kuncung <rickowijaya@gmail.com>
2023-08-15 02:06:44 +0700

    first commit, halfway done

---
+++ src/components/GarpuSendokComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+
+<template>
+  <div class="flex flex-row w-[100vw] justify-center gap-0 p-0">
+    <img src="@/assets/sendok.svg" />
+    <img src="@/assets/garpu.svg" />
+    <img src="@/assets/sendok.svg" />
+    <img src="@/assets/garpu.svg" />
+    <img src="@/assets/sendok.svg" />
+    <img src="@/assets/garpu.svg" />
+    <img src="@/assets/sendok.svg" />
+    <img src="@/assets/garpu.svg" />
+  </div>
+</template>
+
+<style scoped>
+img {
+  scale: 0.5;
+  margin: 0;
+}
+</style>
